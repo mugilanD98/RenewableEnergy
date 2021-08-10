3e569afc2f17a4dab61210a5d6c9cd031e70c529
Mugilan Deiveegan <73863531+mugilanD98@users.noreply.github.com>
2021-08-10 09:29:16 +0530

    Update Renewable_Energy_Monitoring_Dashboard.py

--- Renewable_Energy_Monitoring_Dashboard.py
+++ Renewable_Energy_Monitoring_Dashboard.py
@@ -54,6 +54,9 @@ wind_perc = str(round(wind_perc,1)) + '%'
 st.markdown('''
             <p><h1>Daily Renewable Energy Generation - Monitoring Dashboard</h1><br>
             <h3>All India - RE Generation Summary</h3></p>
+    ''' , unsafe_allow_html=True);            
+            
+st.markdown('''            
             <div style="height:150px;width: 30%;background-color: #d1f0a2; float:left; left: 0px; border-radius: 2px;">
                     <div>
                         <div style="font-family: Arial, Helvetica, sans-serif; font-weight: bold; font-size: 30px; padding: 20px 0px 0px 25px;">'''+str(co2)+''' <span style="font-size: 15px; font-weight: normal">tCO<sub>2</sub></span> </div>
